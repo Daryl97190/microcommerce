4387a2ea34358b90fb4627bba34e984a019451a1
Daryl Livio <daryl.livio@gmail.com>
2021-11-02 17:20:49 +0100

    Ajout du produitController et de sa Dao

--- src/main/java/com/ecommerce/microcommerce/controller/ProduitController.java
+++ src/main/java/com/ecommerce/microcommerce/controller/ProduitController.java
@@ -1,24 +1,38 @@
 package com.ecommerce.microcommerce.controller;
 
+import java.util.List;
+
+import com.ecommerce.microcommerce.dao.ProduitDao;
 import com.ecommerce.microcommerce.model.Produit;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProduitController {
 
+    @Autowired
+    private ProduitDao produitDao;
 
     @GetMapping(value = "/produits")
-    public String listeProduits() {
-        return "Un exemple de produit";
+    public List<Produit> listeProduits() {
+        return produitDao.findAll();
     }
     
     @GetMapping(value = "/produits/{id}")
     public Produit afficherUnProduit(@PathVariable int id) {
         Produit produit = new Produit(id, new String("Aspirateur"), 100);
         return produit;
+    }
+    
+    // ajouter un produit
+    @PostMapping(value = "/produits")
+    public void ajouterProduit(@RequestBody Produit produit) {
+        produitDao.save(produit);
     }
     
 }
--- src/main/java/com/ecommerce/microcommerce/dao/IProduitDao.java
+++ src/main/java/com/ecommerce/microcommerce/dao/IProduitDao.java
@@ -1,24 +1,41 @@
 package com.ecommerce.microcommerce.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.ecommerce.microcommerce.model.Produit;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class IProduitDao implements ProduitDao {
+    
+    public static List<Produit> produits = new ArrayList<>();
+    static {
+        produits.add(new Produit(1, new String("Ordinateur portable"), 350));
+        produits.add(new Produit(2, new String("Aspirateur Robot"), 500));
+        produits.add(new Produit(3, new String("Table de Ping Pong"), 750));
+    }
 
     @Override
     public List<Produit> findAll() {
-        return null;
+        return produits;
     }
 
     @Override
     public Produit findById(int id) {
+        for (Produit produit : produits) {
+            if (produit.getId() == id) {
+                return produit;
+            }
+        }
         return null;
     }
 
     @Override
-    public Produit save(Produit product) {
-        return null;
+    public Produit save(Produit produit) {
+        produits.add(produit);
+        return produit;
     }
     
 }
