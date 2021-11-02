22cb42baf0ed781450411475a7d3fb19c2f5a7d4
Daryl Livio <daryl.livio@gmail.com>
2021-11-02 16:47:25 +0100

    Création du modèle et implémentation des premieres méthodes

--- src/main/java/com/ecommerce/microcommerce/controller/ProduitController.java
+++ src/main/java/com/ecommerce/microcommerce/controller/ProduitController.java
@@ -1,6 +1,9 @@
 package com.ecommerce.microcommerce.controller;
 
+import com.ecommerce.microcommerce.model.Produit;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +13,12 @@ public class ProduitController {
     @GetMapping(value = "/produits")
     public String listeProduits() {
         return "Un exemple de produit";
+    }
+    
+    @GetMapping(value = "/produits/{id}")
+    public Produit afficherUnProduit(@PathVariable int id) {
+        Produit produit = new Produit(id, new String("Aspirateur"), 100);
+        return produit;
     }
     
 }
