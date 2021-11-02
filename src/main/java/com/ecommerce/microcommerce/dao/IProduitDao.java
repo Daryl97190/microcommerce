53bf41e704069108b12dce54206c9b1143a8f856
Daryl Livio <daryl.livio@gmail.com>
2021-11-02 16:54:11 +0100

    Installation de la DAO

---
+++ src/main/java/com/ecommerce/microcommerce/dao/IProduitDao.java
@@ -0,0 +1,24 @@
+package com.ecommerce.microcommerce.dao;
+
+import java.util.List;
+
+import com.ecommerce.microcommerce.model.Produit;
+
+public class IProduitDao implements ProduitDao {
+
+    @Override
+    public List<Produit> findAll() {
+        return null;
+    }
+
+    @Override
+    public Produit findById(int id) {
+        return null;
+    }
+
+    @Override
+    public Produit save(Produit product) {
+        return null;
+    }
+    
+}
